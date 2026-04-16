b80e93a9ea3f1c76529c55441d9ef3894302afb9
wooki0123 <wookimini22@gmail.com>
2026-04-16 15:24:46 +0900

    gitignore-2

---
+++ jsp-board/src/main/java/com/ll/jsp/board/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.ll.jsp.board.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello") //hello
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        resp.setContentType("text/html; charset=UTF-8");
+
+        resp.getWriter().append("<h1>Hello, World!</h1>");
+    }
+}
